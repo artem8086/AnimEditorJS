2cacce307ce74ecbf7970333e9858ae845f399cc
artem8086 <artemsvyatoha@gmail.com>
2019-10-29 11:53:44 +0200

    start make animation editor

---
+++ dist/js/main.js
@@ -0,0 +1,2 @@
+"use strict";(function(){}).call(void 0);
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uY29mZmVlIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJjQUFBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyIiXX0=
